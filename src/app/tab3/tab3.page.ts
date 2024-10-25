d0ef86dd2c6daf48281f7c2c03e55de2b610f37e
Alexis <paulpulamarin141220@gmail.com>
2024-10-25 14:11:31 -0500

    Calcular Fechas Listo

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  fecha1: string;
+  fecha2: string;
+  dias: number | null=null;
+  constructor() {
+    this.fecha1='';
+    this.fecha2='';
+  }
+  calcularDias(){
+    if(this.fecha1 && this.fecha2){
+      const date1 = new Date(this.fecha1);
+      const date2 = new Date(this.fecha2);
 
-  constructor() {}
+      const diferencia = Math.abs(date2.getTime()-date1.getTime());
+
+      this.dias = Math.ceil(diferencia/(1000*3600*24));
+    }else{
+      this.dias = null
+    }
+  }
 
 }
